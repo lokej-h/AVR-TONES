dd7bf75ef339bd4961baa4da7e580af69ffeffb7
lokej <lokej+git@uci.edu>
2020-02-18 10:40:05 -0800

    made the music module

--- GccApplication3/main.c
+++ GccApplication3/main.c
@@ -15,49 +15,12 @@
 
 #define TRUE 1
 #define FALSE 0
-#define LED_ON PORTB |= (1<<PORTB0)
-#define LED_OFF PORTB &= ~(1<<PORTB0)
-#define LED_TOGGLE PINB |= (1<<PINB0)
+// #define LED_ON PORTB |= (1<<PORTB0)
+// #define LED_OFF PORTB &= ~(1<<PORTB0)
+// #define LED_TOGGLE PINB |= (1<<PINB0)
 #define SET_LOW(p,i) p &= !(1<<i)
 #define SET_HIGH(p,i) p |= (1<<i)
-#define NumNotes(x) sizeof(x)/sizeof(Note)
-#define SPK_ON PORTB |= 0x01;
-#define SPK_OFF PORTB |= 0x00;
 
-
-
-typedef struct
-{
-	int freq, duration;
-}Note;
-
-
-
-void PlayNote(Note noteIn)
-{
-	int k =noteIn.duration*noteIn.freq;
-	int t = 1/(2*noteIn.freq);
-	for(int i=0; i<k; ++i)
-	{
-		SPK_ON;
-		small_wait(t);
-		SPK_ON;
-		small_wait(t);
-	}
-}
-void PlaySong(Note* songIn)
-{
-	for(int i=0; i<NumNotes(songIn); ++i)
-	{
-		PlayNote(songIn[i]);
-	}
-}
-
-
-
-	
-	//	  [A]  [As]  [B] [C][Cs] [D] [Ds][E] [Fs][G][Gs]
-//int notes{1136,1073,1012,956,902,851,804,758,716,676,638};
 
 
 int main(void)
@@ -65,51 +28,18 @@ int main(void)
 	//0 for output, 1 for input
 	DDRC = 0x00;
 	PORTC = 0x00;
-	//DDRD |= (1<<DDD0);
-	//unsigned char button=0;
 	
-	/*
-		[1][2][3][4]
-		[5][6][7][8]
-		[9][10][11][12]
-		[13][14][15][16]
-		
-		c0 c1 c2 c3 r0 r1 r2 r3
-	*/
-	//unsigned char i=0;
-	//int i=0;
+	// lcd inits
 	init_time();
 	lcd_init();
 	lcd_clr();
 	lcd_pos(0,0);
-	//button_test();
-	//lcd_puts2("ooooo ");
 	lcd_write_time();
-	DDRA=0x01;
-	DDRB=0x01;
+	
+	
     while (1) 
     {
-		PORTB=0x01;
-		avr_wait(2);
-		PORTB=0x00;
-		avr_wait(2);
-		/*if(PIND & (1<<PIND0))	//if pin 1 in port D is high
-		{
-			button=findWhichPin();
-		}
-		else
-		{
-			LED_OFF;
-			
-			timeTick();
-			lcd_write_time();
-			avr_wait(1000);
-			int button = findWhichPin();
-			if(button !=0)
-			{
-				timeChange(button);
-			}
-		}*/
+		
 	}
 }
 
---
+++ GccApplication3/music.h
@@ -0,0 +1,157 @@
+#ifndef MUSIC
+#define MUSIC
+
+#define SPK_ON PORTB |= 0x01;
+#define SPK_OFF PORTB |= 0x00;
+#define NumNotes(x) sizeof(x)/sizeof(Note)
+
+#define A3	2273
+#define As3	2145
+#define Bf3	2145
+#define B3	2025
+#define C4	1911
+#define Cs4	1804
+#define Df4	1804
+#define D4	1703
+#define Ds4	1607
+#define Ef4	1607
+#define E4	1517
+#define F4	1432
+#define Fs4	1351
+#define Gf4	1351
+#define G4	1276
+#define Gs4	1204
+#define Af4	1204
+#define A4	1136
+#define As4	1073
+#define Bf4	1073
+#define B4	1012
+#define C5	956
+#define Cs5	902
+#define Df5	902
+#define D5	851
+#define Ds5	804
+#define Ef5	804
+#define E5	758
+#define F5	716
+#define Fs5	676
+#define Gf5	676
+#define G5	638
+#define Gs5	602
+#define Af5	602
+
+#define TEMPO 100000ul
+#define W 1.0
+#define H .5
+#define Q .25
+#define Ei .125
+#define S .0625
+
+
+typedef struct
+{
+	int freq, duration;
+}Note;
+
+
+
+void PlayNote(Note noteIn)
+{
+	int k =noteIn.duration*noteIn.freq;
+	int t = 1/(2*noteIn.freq);
+	for(int i=0; i<k; ++i)
+	{
+		SPK_ON;
+		small_wait(t);
+		SPK_ON;
+		small_wait(t);
+	}
+}
+void PlaySong(Note* songIn)
+{
+	for(int i=0; i<NumNotes(songIn); ++i)
+	{
+		PlayNote(songIn[i]);
+	}
+}
+
+
+void note_test(int note)
+{
+    PORTB=0x01;
+    avr_wait_usec(note);
+    PORTB=0x00;
+    avr_wait_usec(note);
+}
+
+	
+	//	  [A]  [As]  [B] [C4][Cs] [D] [Ds][E] [Fs][G][Gs]
+//int notes{1136,1073,1012,956,902,851,804,758,716,676,638};
+
+Note new_song[] ={
+    {C, W},
+    {E, W},
+    {G, W},
+    {B, W},
+    {G, W},
+    {E, W},
+    {C, W},
+};
+
+Note WMIH[52] = {
+	{Bf3, 8},
+	{G4, 16},
+	{G4, 8},
+	{G4, 8},
+	{G4, 8},
+	{G4, 8},
+	{G4, 8},
+	{D4, 8},
+	{Ef4, 8},
+	{G4, 8},
+	{G4, 8},
+	{Ef4, 8},
+	{D4, 2},
+	
+	{Bf3, 8},
+	{G4, 16},
+	{G4, 8},
+	{G4, 8},
+	{G4, 8},
+	{G4, 8},
+	{G4, 8},
+	{D4, 8},
+	{Ef4, 8},
+	{D4, 8},
+	{Bf4, 8},
+	{A4, 8},
+	{G3, 2},
+	
+	{Bf3, 8},
+	{G4, 16},
+	{G4, 8},
+	{G4, 8},
+	{G4, 8},
+	{G4, 8},
+	{G4, 8},
+	{D4, 8},
+	{Ef4, 8},
+	{G4, 8},
+	{G4, 8},
+	{Ef4, 8},
+	{D4, 2},
+	
+	{Bf3, 8},
+	{G4, 16},
+	{G4, 8},
+	{G4, 8},
+	{G4, 8},
+	{G4, 8},
+	{G4, 8},
+	{D4, 8},
+	{Ef4, 8},
+	{D4, 8},
+	{Bf4, 8},
+	{A4, 8},
+	{G3, 2}
+}
